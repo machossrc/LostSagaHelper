37f28d01dd8a45f17a8c8c910ec8f835395c7d66
Admin <Admin@DESKTOP-AS4TQ20>
2024-04-05 05:58:44 +0900

    ok

--- ioBaseChar.h
+++ ioBaseChar.h
@@ -1,6 +1,13 @@
 #pragma once
 class ioBaseChar
 {
+private:
+	char Buffer1[100]; //필요 없는 구조체의 크기 만큼 Buffer 의 크기를 증가 시킨다.
+
+	DWORD aa; //필요한 정보를 가져오고
+
+	char Buffer2[24]; //버퍼를 증가 시킨다
+
 public:
 	ioBaseChar();
 	~ioBaseChar();
