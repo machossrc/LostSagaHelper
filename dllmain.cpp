3532f5e3b266e4894b0dfb114ada90f0940e1d80
Admin <Admin@DESKTOP-AS4TQ20>
2024-04-05 05:38:49 +0900

    초기 커밋.

---
+++ dllmain.cpp
@@ -0,0 +1,22 @@
+﻿#include "StdAfx.h"
+
+using namespace std;
+
+void OnHelper()
+{
+
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)OnHelper, NULL, NULL, NULL);
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
